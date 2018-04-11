cff502bdf50d41b22c1c4695a5ff1b1527709385
Plasmoxy <sebohacker@gmail.com>
2018-04-11 18:56:53 +0200

    mya

---
+++ MyApplication/app/src/main/java/io/github/plasmoxy/myapplication/Other.kt
@@ -0,0 +1,15 @@
+package io.github.plasmoxy.myapplication
+
+import android.util.Log
+
+class Other {
+
+	companion object {
+		var msg = "XDDDDD"
+
+		fun hello() {
+			Log.d("Hello i say : ", msg )
+		}
+	}
+
+}
