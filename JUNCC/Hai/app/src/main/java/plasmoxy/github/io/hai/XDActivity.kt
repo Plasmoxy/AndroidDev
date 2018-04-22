03f5cc51b14ea1385eb8d9dd11f79b2e844146c2
Plasmoxy <31391787+Plasmoxy@users.noreply.github.com>
2018-04-22 15:13:39 +0200

    adde

---
+++ JUNCC/Hai/app/src/main/java/plasmoxy/github/io/hai/XDActivity.kt
@@ -0,0 +1,14 @@
+package plasmoxy.github.io.hai
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+
+class XDActivity : AppCompatActivity() {
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.xd_layout)
+	}
+
+}
