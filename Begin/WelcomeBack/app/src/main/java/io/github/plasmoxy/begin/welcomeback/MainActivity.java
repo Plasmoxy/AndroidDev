23997b01c7ad267fa5fe4f527dd9557f73cbab4f
Plasmoxy <sebohacker@gmail.com>
2018-06-17 15:51:37 +0200

    welcome back

---
+++ Begin/WelcomeBack/app/src/main/java/io/github/plasmoxy/begin/welcomeback/MainActivity.java
@@ -0,0 +1,13 @@
+package io.github.plasmoxy.begin.welcomeback;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
