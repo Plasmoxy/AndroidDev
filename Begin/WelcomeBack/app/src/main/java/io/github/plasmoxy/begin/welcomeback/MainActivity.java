fcbbbe5fb31368f4dbf8ac57281e3f9036a27e63
Plasmoxy <31391787+Plasmoxy@users.noreply.github.com>
2018-06-18 16:58:58 +0200

    ahahah welcome

--- Begin/WelcomeBack/app/src/main/java/io/github/plasmoxy/begin/welcomeback/MainActivity.java
+++ Begin/WelcomeBack/app/src/main/java/io/github/plasmoxy/begin/welcomeback/MainActivity.java
@@ -2,6 +2,10 @@ package io.github.plasmoxy.begin.welcomeback;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		final ImageView image = findViewById(R.id.imageView);
+		final Button b = findViewById(R.id.button);
+
+		final TextView text = findViewById(R.id.textView);
+		final Button generat = findViewById(R.id.generat);
+
+		b.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				image.setVisibility(View.VISIBLE);
+			}
+		});
+
 	}
 }
