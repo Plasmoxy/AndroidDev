b94250fac88eccaf5d32936d60d7d10bd7792efb
Plasmoxy <sebohacker@gmail.com>
2018-06-24 14:24:27 +0200

    i really like this

---
+++ Begin/LifeCycle/app/src/main/java/io/github/plasmoxy/begin/lifecycle/MainActivity.kt
@@ -0,0 +1,56 @@
+package io.github.plasmoxy.begin.lifecycle
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.*
+import kotlin.properties.Delegates.observable
+
+class MainActivity : AppCompatActivity() {
+    
+    // observables
+    
+    var info : String by observable("XD") { _, _, newVal ->
+        infoTextView.text = newVal
+    }
+    
+    // listeners
+    
+    var randomVal = View.OnClickListener {
+        info = (Math.random() * 10).toInt().toString()
+    }
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        
+        mainButton.setOnClickListener(randomVal)
+        
+        toastButton.setOnClickListener { 
+            toast("SOME TOAST")
+        }
+        
+        dialogButton.setOnClickListener {
+            alert("fokin answer boi", "YOUR MOM GEY?") {
+                yesButton { toast("HA GAYAYYYYY") }
+                noButton { toast("VERY WELL") }
+            }.show()
+        }
+        
+        selectorButton.setOnClickListener {
+            val countries = listOf("Russia", "USA", "Japan", "Australia")
+            
+            selector("Where are you from?", countries) { dialogInterface, i ->
+                toast("So you're living in ${countries[i]}, right?")
+            }
+        }
+        
+        progressButton.setOnClickListener {
+            val dialog = progressDialog(message = "Please wait a bit…", title = "Fetching data")
+            
+        }
+        
+        toast("app started")
+    }
+}
