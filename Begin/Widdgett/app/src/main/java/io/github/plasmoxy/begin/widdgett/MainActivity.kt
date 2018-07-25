d0ad7b8b69c9fa41d799243bfe6ac932abb56a9a
Plasmoxy <sebohacker@gmail.com>
2018-07-25 23:53:43 +0200

    hm

--- Begin/Widdgett/app/src/main/java/io/github/plasmoxy/begin/widdgett/MainActivity.kt
+++ Begin/Widdgett/app/src/main/java/io/github/plasmoxy/begin/widdgett/MainActivity.kt
@@ -1,18 +1,28 @@
 package io.github.plasmoxy.begin.widdgett
 
 import android.os.Bundle
+import android.os.Handler
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    
+    private val scrollToCardListener = View.OnFocusChangeListener {v, hasFocus ->
+        if (hasFocus) {
+            coreScroll.post {
+                coreScroll.smoothScrollTo(0, cardViewLogin.bottom)
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         
-        
+        editTextName.setOnFocusChangeListener(scrollToCardListener)
+        editTextPassword.setOnFocusChangeListener(scrollToCardListener)
     }
     
 }
