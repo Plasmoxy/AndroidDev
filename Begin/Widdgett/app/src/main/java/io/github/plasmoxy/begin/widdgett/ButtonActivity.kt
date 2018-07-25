d853b2b3783062ab0a58315485481505d65f5a7d
Plasmoxy <sebohacker@gmail.com>
2018-07-25 23:26:52 +0200

    hmhh

--- Begin/Widdgett/app/src/main/java/io/github/plasmoxy/begin/widdgett/ButtonActivity.kt
+++ Begin/Widdgett/app/src/main/java/io/github/plasmoxy/begin/widdgett/ButtonActivity.kt
@@ -2,17 +2,16 @@ package io.github.plasmoxy.begin.widdgett
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.View
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class ButtonActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_button)
         
-        
+        editTextName.setOnFocusChangeListener { v, hasFocus ->
+            
+        }
     }
-    
 }
