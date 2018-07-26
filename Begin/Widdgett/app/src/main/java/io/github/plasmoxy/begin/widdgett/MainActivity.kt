72ea804acfed6c438b6b53ccf59aa1f8e50060ce
Plasmoxy <sebohacker@gmail.com>
2018-07-26 16:31:28 +0200

    good

--- Begin/Widdgett/app/src/main/java/io/github/plasmoxy/begin/widdgett/MainActivity.kt
+++ Begin/Widdgett/app/src/main/java/io/github/plasmoxy/begin/widdgett/MainActivity.kt
@@ -2,10 +2,8 @@ package io.github.plasmoxy.begin.widdgett
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
 import android.support.v7.app.AppCompatActivity
 import android.view.View
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
