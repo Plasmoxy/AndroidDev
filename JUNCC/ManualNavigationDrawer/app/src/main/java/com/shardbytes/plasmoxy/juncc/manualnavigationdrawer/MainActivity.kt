3f6a570ff2fc74ec3939110f06b0594c09402618
Plasmoxy <sebohacker@gmail.com>
2018-08-16 23:10:11 +0200

    pretty well made drawer

--- JUNCC/ManualNavigationDrawer/app/src/main/java/com/shardbytes/plasmoxy/juncc/manualnavigationdrawer/MainActivity.kt
+++ JUNCC/ManualNavigationDrawer/app/src/main/java/com/shardbytes/plasmoxy/juncc/manualnavigationdrawer/MainActivity.kt
@@ -1,15 +1,40 @@
 package com.shardbytes.plasmoxy.juncc.manualnavigationdrawer
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.util.AttributeSet
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false) // disable app title
         
         
+        val toggle = ActionBarDrawerToggle(this, drawer_layout, toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+    }
+    
+
+    // close navigation drawer with back button
+    override fun onBackPressed() {
         
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed() // closes activity and stuff
+        }
     }
 }
