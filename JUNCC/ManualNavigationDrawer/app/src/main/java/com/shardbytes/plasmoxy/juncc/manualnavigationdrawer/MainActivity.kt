1d1d3367640ac7c0b4894d77227646ef882b09e1
Plasmoxy <sebohacker@gmail.com>
2018-08-18 20:39:54 +0200

    modfragment

--- JUNCC/ManualNavigationDrawer/app/src/main/java/com/shardbytes/plasmoxy/juncc/manualnavigationdrawer/MainActivity.kt
+++ JUNCC/ManualNavigationDrawer/app/src/main/java/com/shardbytes/plasmoxy/juncc/manualnavigationdrawer/MainActivity.kt
@@ -1,25 +1,61 @@
 package com.shardbytes.plasmoxy.juncc.manualnavigationdrawer
 
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.app.Fragment
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false) // disable app title
         
+        // method below
+        nav_view.setNavigationItemSelectedListener(this)
         
+        // setup hamburger
         val toggle = ActionBarDrawerToggle(this, drawer_layout, toolbar,
                 R.string.navigation_drawer_open,
                 R.string.navigation_drawer_close)
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
+        
+        // set fragment if activity is freshly created, if its something like device rotate or stuff, we keep the selected item and fragment !
+        if (savedInstanceState == null) {
+            switchFragment(AccountFragment())
+            nav_view.setCheckedItem(R.id.nav_account)
+        }
+    }
+
+    fun switchFragment(f: Fragment) =
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, f)
+                    .commit()
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            
+            R.id.nav_account -> switchFragment(AccountFragment())
+            R.id.nav_info -> switchFragment(InfoFragment())
+            R.id.nav_photo -> switchFragment(PhotoFragment())
+            
+            R.id.nav_message -> toast("Hello !")
+            R.id.nav_faggot -> toast("YOR A FAGGOT")
+            
+        }
+        
+        drawer_layout.closeDrawer(GravityCompat.START)
+        
+        return true
     }
     
 
@@ -32,4 +68,5 @@ class MainActivity : AppCompatActivity() {
             super.onBackPressed() // closes activity and stuff
         }
     }
+    
 }
