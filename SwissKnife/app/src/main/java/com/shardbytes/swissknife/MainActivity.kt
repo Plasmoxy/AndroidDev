ad8dea0586939faf6b34c933dfcc8b5bb40d9ace
Plasmoxy <sebohacker@gmail.com>
2018-10-09 20:29:43 +0200

    tls 1.2 enabled 4.4

--- SwissKnife/app/src/main/java/com/shardbytes/swissknife/MainActivity.kt
+++ SwissKnife/app/src/main/java/com/shardbytes/swissknife/MainActivity.kt
@@ -1,16 +1,43 @@
 package com.shardbytes.swissknife
 
+import android.content.Context
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.common.GoogleApiAvailability
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException
+import com.google.android.gms.common.GooglePlayServicesRepairableException
+import com.google.android.gms.security.ProviderInstaller
 import com.shardbytes.swissknife.activities.FuelConnections
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.toast
+
+fun Context.installTls12() {
+    try {
+        ProviderInstaller.installIfNeeded(this)
+    } catch (e: GooglePlayServicesRepairableException) {
+        // Prompt the user to install/update/enable Google Play services.
+        GoogleApiAvailability.getInstance()
+                .showErrorNotification(this, e.connectionStatusCode)
+    } catch (e: GooglePlayServicesNotAvailableException) {
+        
+    }
+}
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        // install TLS 1.2 using gms if on < Lolipop
+        
+        toast("API=${Build.VERSION.SDK_INT}")
+        
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            installTls12()
+        }
         
         buttonFuelConnections.setOnClickListener { startActivity<FuelConnections>() }
     }
--- SwissKnife/app/src/main/java/com/shardbytes/swissknife/activities/FuelConnections.kt
+++ SwissKnife/app/src/main/java/com/shardbytes/swissknife/activities/FuelConnections.kt
@@ -1,11 +1,14 @@
 package com.shardbytes.swissknife.activities
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import com.github.kittinunf.fuel.httpGet
 import com.shardbytes.swissknife.R
 import kotlinx.android.synthetic.main.activity_fuel_connections.*
+import okhttp3.*
 import org.jetbrains.anko.getStackTraceString
+import org.jetbrains.anko.toast
+import java.io.IOException
 
 class FuelConnections : AppCompatActivity() {
 
@@ -13,15 +16,43 @@ class FuelConnections : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fuel_connections)
         
+        val host = "https://shardbytes.com:10101"
+        
+        val client = OkHttpClient.Builder()
+                .build()
+        
         buttonFuelGet.setOnClickListener { view -> 
             
-            "https://shardbytes.com:10101/api/shop/getrecord/root".httpGet().responseString { _, _, result ->
+            "$host/api/shop/getrecord/root".httpGet().responseString { _, _, result ->
                 result.fold({
                     textViewInfo.setText(it)
                 },{
                     textViewInfo.setText(it.getStackTraceString())
                 })
             }
+            
+        }
+        
+        buttonOkHttpGet.setOnClickListener {
+            
+            val request = Request.Builder()
+                    .url("$host/api/shop/getrecord/root")
+                    .get()
+                    .build()
+            
+            client.newCall(request).enqueue(object : Callback {
+                
+                override fun onFailure(call: Call, e: IOException) {
+                    e.printStackTrace()
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    runOnUiThread {
+                        toast("response")
+                        textViewInfo.setText(response.body()?.string() ?: "NUL")
+                    }
+                }
+            })
             
         }
         
